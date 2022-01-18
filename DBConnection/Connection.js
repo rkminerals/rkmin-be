c2c7f2bb3a575dad1543c34d4c8f9ccd475cc437
Neelesh Sharma <43935540+nodaddy@users.noreply.github.com>
2022-01-19 01:34:17 +0530

    Update Connection.js

--- DBConnection/Connection.js
+++ DBConnection/Connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const ConnectionString = "mongodb+srv://mongodb:mongodb@cluster0.pfafq.mongodb.net/rkmin?retryWrites=true&w=majority";
+const ConnectionString = "mongodb+srv://rkminerals:rkminerals1234@cluster0.sdi9e.mongodb.net/rkmin?retryWrites=true&w=majority";
+
 
 const connectDB = async () => {
     await mongoose.connect(ConnectionString, {
